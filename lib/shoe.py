4e6a13f494a0ffe8c8ae19a388589304fbb7c2f7
splitlang <seanmark.student@moringaschool>
2024-06-18 00:34:07 +0300

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"  # Initially, the condition is set to "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"  # Setting condition to "New" after repair
+        print("Your shoe is as good as new!")
